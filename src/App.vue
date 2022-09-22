61a88afbba577de938b69f663ac2207a7f454c66
JustInTimez <just.oost@gmail.com>
2022-09-22 15:52:39 +0200

    Added target_blank to footer links
    Forgot to make these open in a new tab. Done!

--- src/App.vue
+++ src/App.vue
@@ -27,15 +27,15 @@ import { RouterLink, RouterView } from 'vue-router'
       <div class="col">
         <ul class="nav flex-column footer-links">
           <li class="nav-item mb-2"><a href="https://help.netflix.com/support/412" target="_blank" class="nav-link p-0 text-muted">FAQ</a></li>
-          <li class="nav-item mb-2"><a href="https://help.netflix.com/legal/termsofuse" class="nav-link p-0 text-muted">Terms of Use</a></li>
-          <li class="nav-item mb-2"><a href="https://www.netflix.com/za/login#" class="nav-link p-0 text-muted">Cookie Preferences</a></li>
+          <li class="nav-item mb-2"><a href="https://help.netflix.com/legal/termsofuse" target="_blank" class="nav-link p-0 text-muted">Terms of Use</a></li>
+          <li class="nav-item mb-2"><a href="https://www.netflix.com/za/login#" target="_blank" class="nav-link p-0 text-muted">Cookie Preferences</a></li>
         </ul>
       </div>
       <div class="col">
         <ul class="nav flex-column footer-links">
-          <li class="nav-item mb-2"><a href="https://help.netflix.com/" class="nav-link p-0 text-muted">Help Center</a></li>
-          <li class="nav-item mb-2"><a href="https://help.netflix.com/legal/privacy" class="nav-link p-0 text-muted">Privacy</a></li>
-          <li class="nav-item mb-2"><a href="https://help.netflix.com/legal/corpinfo" class="nav-link p-0 text-muted">Corporate Information</a></li>
+          <li class="nav-item mb-2"><a href="https://help.netflix.com/" target="_blank" class="nav-link p-0 text-muted">Help Center</a></li>
+          <li class="nav-item mb-2"><a href="https://help.netflix.com/legal/privacy" target="_blank" class="nav-link p-0 text-muted">Privacy</a></li>
+          <li class="nav-item mb-2"><a href="https://help.netflix.com/legal/corpinfo" target="_blank" class="nav-link p-0 text-muted">Corporate Information</a></li>
         </ul>
       </div>
     </div>
@@ -43,12 +43,6 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
-
-template {
-  background-image: url('../public/images/login-bg.jpg');
-  background-color: black;
-}
-
 header {
   line-height: 1.5;
   max-height: 100vh;
