9c1491d04c859da8f49bd742926472b7b28338af
JustInTimez <just.oost@gmail.com>
2022-10-05 15:52:45 +0200

    Adding icons and styling to movie previews

--- src/components/MoviePreview.vue
+++ src/components/MoviePreview.vue
@@ -24,20 +24,40 @@ export default {
 </script>
 
 <template>
+    <!-- <div class="card resting" :style="backgroundStyle" @mouseover="play()" @mouseout="pause()">
+        <img src="..." class="card-img-top" alt="movies!">
+        <video class="card-body p-0 card-img-top" alt="movies!" 
+        ref="video"
+            :muted="muted"
+                src="/videos/placeholder.mp4"
+                loop
+        ></video>
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div> -->
     <div class="resting"
         :style="backgroundStyle"
         @mouseover="play()"
         @mouseout="pause()"
     >
-        <div class="preview">
-            <video 
-               ref="video"
-                :muted="muted"
-                    src="/videos/placeholder.mp4"
-                    loop
-            ></video>
-            <button @click="toggleMute()">{{this.muted ? 'Unmute' : 'Mute'}}</button>
-            <span class="label">{{this.label}}</span>
+        <div class="card preview bg-dark">
+            <div class="card-body">
+                <video class="card-body p-0" 
+                ref="video"
+                    :muted="muted"
+                        src="/videos/placeholder.mp4"
+                        loop
+                ></video>
+                <button @click="toggleMute()">{{this.muted ? "Unmute" : "Mute"}}</button>
+                <button @click="wishlistAdd()" class="wishlistButt"><img src="/images/add-new.svg"></button>
+                <button @click="wishlistRemove()" class="wishlistButt2"><img src="/images/minus.svg"></button>
+
+                <span class="label">{{this.label}}</span>
+            </div>
         </div>
     </div>
 </template>
