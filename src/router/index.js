5baa6061080da56f457a35d0b6deabd0f7543959
Justin Oosthuizen <just.oost@gmail.com>
2022-10-13 22:47:47 +0200

    Amended history function in router

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   /** Basic routing structure  */
   routes: [
     {
